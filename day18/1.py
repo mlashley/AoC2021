1e75ff27fc2ec67c8b4de0ad9f7942ccb3f0b64a
Malcolm Lashley <mlashley@gmail.com>
2021-12-18 13:27:34 +0000

    WIP

--- day18/1.py
+++ day18/1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3 
+import re
 
 f = open("input","r")
 f = open("input.test","r")
@@ -32,18 +33,10 @@ assert depth([[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]) == 4
 def deep(n,depth,lval,rval):
     print(f"deep d:{depth} {n} lval:{lval} rval:{rval}")
     
-    if depth == 4:
-        newr=None
-        newl=None 
-        if lval != None:
-            newl = lval+n[0]
-
-        if rval != None:
-            newr = rval+n[1]
-        
-        print(f"new {newl},{newr}  from {lval}+{n[0]} and {rval}+{n[1]}")
+    if depth == 4:       
+        print(f"Explode {n[0]},{n[1]}")
         # Replace pair with zero
-        return newl,newr,True
+        return n[0],n[1],True
 
     if type(n[0]) == int:
         lval = n[0]
@@ -54,22 +47,9 @@ def deep(n,depth,lval,rval):
         newl,newr,zapme = deep(n[0],depth+1,lval,rval)
 
         if zapme:
-            n[0] = 0
+            n[0] = f"MARKER_{newl}_{newr}_MARKER"
             zapme = False
 
-        if newr != None:
-            if type(n[1]) == int:
-                    print(f"R1-Replace {n[1]} with {newr}")
-                    n[1] = newr
-                    newr=None # stop filter-up
-            
-        if newl != None:
-            if type(n[0]) == int:
-                    print(f"L1-Replace {n[0]} with {newl}")
-                    n[1] = newl
-                    newl=None # stop filter-up
-
-        # Done
         print(f"return from left {newl} {newr} {zapme}")
         return newl,newr,zapme
 
@@ -77,25 +57,14 @@ def deep(n,depth,lval,rval):
         newl,newr,zapme = deep(n[1],depth+1,lval,rval)
 
         if zapme:
-            n[1] = 0
+            n[1] = f"MARKER_{newl}_{newr}_MARKER"
             zapme = False
 
-        if newl != None:
-            if type(n[0]) == int:
-                    print(f"L2-Replace {n[0]} with {newl}")
-                    n[0] = newl
-                    newl=None # stop filter-up
-            
-        if newl != None:
-            if type(n[0]) == int:
-                    print(f"L2-Replace {n[0]} with {newl}")
-                    n[1] = newl
-                    newl=None # stop filter-up
         # Done
         print(f"return from right {newl} {newr} {zapme}")
         return newl,newr,zapme
 
-    print(f"return from end  {newl} {newr} {zapme}")
+    print(f"return from end d{depth}")
     return None,None,False # lval, rval, zapme
 
 def explodeNum(n):
@@ -103,20 +72,51 @@ def explodeNum(n):
     print(f"Before: {qq}")
     deep(qq,0,None,None)
     print(f"After: {qq}")
-    return qq
+    s = str(qq)
+    m = re.search("(.*)'MARKER_(\d)_(\d)_MARKER'(.*)",s)
+    if m:
+        pre  = m.group(1)
+        addl = int(m.group(2))
+        addr = int(m.group(3))
+        post  = m.group(4)
+        print("Before: PRE:",pre,"Nums:",addl,addr,"POST:",post)
+
+        # Add addl to last number in pre (if any)
+        m = re.search(r".*(\d+)", pre)
+        if m:
+            st = m.span(1)[0]
+            ed = m.span(1)[1]
+            replace = int(pre[st:ed]) + addl
+            pre = pre[:st] + str(replace) + pre[ed:]
+
+        # add addr to first number in post (if any)
+        m = re.search(r"(\d+)", post)
+        if m:
+            st = m.span(1)[0]
+            ed = m.span(1)[1]
+            replace = int(post[st:ed]) + addr
+            post = post[:st] + str(replace) + post[ed:]
+    
+        print("After: PRE:",pre,"Nums:",addl,addr,"POST:",post)
+
+        # Replace exploder with Zero
+        s= pre + "0" + post
+    print("End Explode:",s)
+    return eval(s)
     
 
 
 # deep([[[[[9,8],1],2],3],4],depth=0,lval=0,rval=0)
 
     
-
-assert explodeNum([[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]) == [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]
-
-assert explodeNum([[[[[9,8],1],2],3],4]) == [[[[0,9],2],3],4]
 assert explodeNum([7,[6,[5,[4,[3,2]]]]]) == [7,[6,[5,[7,0]]]]
+print("-----------------")
+assert explodeNum([[[[[9,8],1],2],3],4]) == [[[[0,9],2],3],4]
+print("-----------------")
 assert explodeNum([[6,[5,[4,[3,2]]]],1]) == [[6,[5,[7,0]]],3]
-
+print("-----------------")
+assert explodeNum([[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]) == [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]
+print("-----------------")
 assert explodeNum([[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]) == [[3,[2,[8,0]]],[9,[5,[7,0]]]]
 
 
